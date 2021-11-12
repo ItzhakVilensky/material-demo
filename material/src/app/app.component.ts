d00537a02b262709dd85020ce89494705db18410
Itzhak Vilensky <itzhak.vil@gmail.com>
2021-11-12 14:20:01 +0200

    * MatSidenavModule within: - mat-drawer-container - mat-drawer
    -mat-sidenav-container
    -mat-sidenav
    -mat-sidenav-content
    
    *Add FormsModule it does not work though
    * Add to AppComponent:
    -opened
    -showFiller
    -log()
    
    * Add styling
    - mat-sidenav-container
    - mat-sidenav
    - mat-sidenav-content
    - mat-drawer-container
    -mat-drawer

--- material/src/app/app.component.ts
+++ material/src/app/app.component.ts
@@ -9,11 +9,17 @@ export class AppComponent {
   title = 'material';
   notifications = 0;
   showSpinner = false;
+  opened = false;
+  showFiller = false;
 
   loadData() {
     this.showSpinner = true;
     setTimeout(() => {
       this.showSpinner = false;
     }, 3000);
+  }
+
+  log(state: string) {
+    console.log('state: ', state);
   }
 }
