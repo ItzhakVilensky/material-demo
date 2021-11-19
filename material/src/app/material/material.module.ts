7bbc83a90706b21aaab19701ff24352a929584d9
Itzhak Vilensky <itzhak.vil@gmail.com>
2021-11-19 15:58:06 +0200

    * MatTableModule interface PeriodicElement ELEMENT_DATA: PeriodicElement[] displayedColumns dataSource
    mat-table
    
      <ng-container matColumnDef="position">
        <th mat-header-cell *matHeaderCellDef> No. </th>
        <td mat-cell *matCellDef="let element"> {{element.position}} </td>
      </ng-container>

--- material/src/app/material/material.module.ts
+++ material/src/app/material/material.module.ts
@@ -25,6 +25,7 @@ import { MatNativeDateModule } from '@angular/material/core';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatDialogModule } from '@angular/material/dialog';
+import { MatTableModule } from '@angular/material/table';
 
 const MaterialComponents = [
   MatButtonModule,
@@ -52,7 +53,8 @@ const MaterialComponents = [
   MatNativeDateModule,
   MatTooltipModule,
   MatSnackBarModule,
-  MatDialogModule
+  MatDialogModule,
+  MatTableModule
 ];
 
 @NgModule({
